45b50916a195dc6aa7057ab9157e8c6c2d3de940
Arvind Chauhan <40683239+arvind520@users.noreply.github.com>
2022-05-03 16:12:34 +0530

    ContextApi is applied

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import Navbar from "./components/layout/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen ">
-        <Navbar />
-        <main className="px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen ">
+          <Navbar />
+          <main className="px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
