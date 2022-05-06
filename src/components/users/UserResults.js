ab1ec0291f3ccd3a564ada4a1dddd8a72f4f5aff
Arvind Chauhan <40683239+arvind520@users.noreply.github.com>
2022-05-06 12:01:42 +0530

    searchUser is added

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,19 +1,15 @@
-import React, { useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import GithubContext from "../../context/github/GithubContext";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
 const UserResults = () => {
-  const { users, loading, fetchUsers } = useContext(GithubContext);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const { users, loading } = useContext(GithubContext);
 
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-        {users.map((user, index) => (
+        {users.map((user) => (
           <UserItem user={user} key={user.id} />
         ))}
       </div>
